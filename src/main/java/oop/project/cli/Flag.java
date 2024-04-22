a616f305fd2e4a0cc49869e51186a01ed20302c4
Susana Lugones <117051904+SusanaLugones-ufl@users.noreply.github.com>
2024-04-22 19:44:43 -0400

    Updated Flag.java

--- src/main/java/oop/project/cli/Flag.java
+++ src/main/java/oop/project/cli/Flag.java
@@ -6,10 +6,15 @@ public class Flag {
 
     private final String name;
     private final Object arg;
+    private Object value;
 
     Flag(String name, Object arg) {
         this.name = name;
         this.arg = arg;
+    }
+
+    public Object getValue() {
+        return this.value;
     }
 
     public String getName() {
